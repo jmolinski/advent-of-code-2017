a4571cc40feb3a264819614e763af8e969b1cc8c
Jakub Molinski <kubamolinski@gmail.com>
2017-12-02 09:33:17 +0100

    day 2 solutions

---
+++ day-2/main.py
@@ -0,0 +1,17 @@
+import itertools
+
+spreadsheet = '''5 9 2 8
+9 4 7 3
+3 8 6 5'''
+
+matrix = [list(map(int, row.split())) for row in spreadsheet.split('\n')]
+
+answer_part_1 = sum(max(row) - min(row) for row in matrix)
+
+answer_part_2 = sum(
+    max([a // b, b // a])
+    for row in matrix for (a, b) in itertools.combinations(row, 2)
+    if a % b == 0 or b % a == 0
+)
+
+print(answer_part_1, answer_part_2)
