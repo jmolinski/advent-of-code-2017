e5a784e6fe28ba75e8e52d9f98354120d8121fca
Jakub Molinski <kubamolinski@gmail.com>
2017-12-11 21:08:06 +0100

    day 11 solutions

---
+++ day-11/main.py
@@ -0,0 +1,19 @@
+from itertools import accumulate
+import operator as op
+
+steps = open('data.txt', 'r').read().split(',')
+
+directions = {'n': (0, 2), 's': (0, -2), 'nw': (-1, 1),
+              'ne': (1, 1), 'sw': (-1, -1), 'se': (1, -1)}
+
+distances = [  # distances from (0, 0) in consecutive steps
+    abs(x) + (max(0, abs(y) - abs(x))) // 2
+    for x, y in
+    accumulate((directions[d] for d in steps),
+               lambda v1, v2: list(map(op.add, v1, v2)))
+]
+
+answer_part_1 = distances[-1]
+answer_part_2 = max(dst for dst in distances)
+
+print(answer_part_1, answer_part_2)
